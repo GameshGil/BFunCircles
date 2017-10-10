d8a17abd828d1191d6a5987a1a0944804beeacac
Gamesh <s.amirov90@mail.ru>
2017-10-10 11:15:45 +0400

    GemSpawn added

---
+++ Assets/Scripts/HeroScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour 
+{
+    public float speed = 10f;
+
+    private Rigidbody2D myBody;
+
+    public int scores = 0;
+
+	void Start () 
+	{
+        myBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () 
+	{
+        float xMove = Input.GetAxis("Horizontal") * speed;
+        float yMove = Input.GetAxis("Vertical") * speed;
+        myBody.AddForce(new Vector2(xMove, yMove));
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Gem")
+        {
+            scores++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour 
+{
+    public GameObject spawnField;
+    public GameObject spawnObject;
+
+    public int count;
+
+    private float offset;
+    private float maxX, maxY;
+    private float x, y;
+
+    void Start () 
+	{
+        //Получение области спавна, коллайдера области спавна и объекта спавна
+        SpriteRenderer spawnFieldSprite = spawnField.GetComponent<SpriteRenderer>();
+        BoxCollider2D fieldCollider = spawnField.GetComponent<BoxCollider2D>();
+        SpriteRenderer spawnObjectSprite = spawnObject.GetComponent<SpriteRenderer>();
+        
+        //Получение отступа
+        offset = Mathf.Min(fieldCollider.size.x, fieldCollider.size.y) +
+            Mathf.Max(spawnObjectSprite.bounds.size.x, spawnObjectSprite.bounds.size.y) / 2;
+        maxX = spawnFieldSprite.bounds.size.x / 2 - offset;
+        maxY = spawnFieldSprite.bounds.size.y / 2 - offset;
+        //Спавн объектов
+        Spawn(count);
+    }
+
+    //Описание функции спавна
+    public void Spawn(int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            x = Random.Range(-maxX, maxX);
+            y = Random.Range(-maxY, maxY);
+            Instantiate(spawnObject, new Vector3(x, y, 0), Quaternion.identity);
+        }
+    }
+}
