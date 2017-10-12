0d9cb4fb68b011880cfda501ee7c1415801c029f
Gamesh <s.amirov90@mail.ru>
2017-10-12 23:27:19 +0400

    win text

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameScript : MonoBehaviour 
+{
+    public GameObject player;
+    public Text scoreText;
+    public Text looseText;
+    public int maxScore;
+    private HeroScript heroscript;
+
+	void Start () 
+	{
+        heroscript = player.GetComponent<HeroScript>();
+	}
+	
+	void Update () 
+	{
+        scoreText.text = heroscript.scores.ToString() + " / " + maxScore.ToString();
+
+        if (heroscript.scores >= maxScore)
+        {
+            looseText.text = "Ты победил!";
+            SceneManager.LoadScene(0);
+        }
+	}
+}
